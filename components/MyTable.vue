0fa82b49fcbe6ff46a721411888c5a60102ddd0a
Isaac Vorköper <isaaclennart@gmail.com>
2021-09-02 08:23:11 +0200

    Mainpage layout done.

---
+++ components/MyTable.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+    <tr>
+        <td>{{ manufacturer.manufacturerId }}</td>
+        <td>{{ manufacturer.name }}</td>
+        <td><button type="button"
+                  class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                 fill="currentColor" aria-hidden="true"><path
+              d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>
+            </svg>
+            Edit
+        </button></td>
+        <td><button type="button"
+         class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+            </svg>
+                  Delete
+        </button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default{
+props: {
+    manufacturer: {
+      type: Object,
+      required: true,
+        }
+    }
+}
+</script>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td, th {
+  border: 1px solid #bbbbbb;
+  text-align: left;
+  padding: 8px;
+}
+tr:nth-child(even) {
+  background-color: #e3e3e3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,55 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2>Manufacturer List</h2>
+
+    <table id="MyTable" class="table-fixed">
+      <tr>
+        <th>ID</th>
+        <th class="w-1/2">Manufacturer</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      <MyTable v-for="manufacturer in manufacturers" :key="manufacturer.manufacturerId" :manufacturer="manufacturer"/>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {}
+import MyTable from "../components/MyTable"
+export default {
+  components: {
+    MyTable
+  },
+  data() {
+    return {
+      manufacturers: []
+    }
+  },
+  head() {
+    return {
+      title: "Manufacturer List",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Fufugiganten data"
+        }
+      ]
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+    try {
+      const res = await this.$axios.get('https://localhost:44319/api/Manufacturers', config);
+      console.log(res.data)
+      this.manufacturers = res.data;
+    } catch (err) {
+      console.log(err)
+    }
+  }
+};
 </script>
